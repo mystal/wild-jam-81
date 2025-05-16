0940c0b7fd521e98c7cc83f6485ce8fd0d5df59a
jbuehler23 <jbuehler23@gmail.com>
2025-05-16 07:17:11 +0100

    feat: add scene transition - breaking bug with level transitions as the AnimationPlayer is null onready

--- globals/level_manager.gd
+++ globals/level_manager.gd
@@ -26,7 +26,7 @@ func load_new_level(
 	target_transition = _target_transition
 	position_offset = _position_offset
 	
-	await get_tree().process_frame # level transition
+	await SceneTransition.fade_out()
 	
 	level_load_started.emit()
 	
@@ -34,7 +34,7 @@ func load_new_level(
 	
 	get_tree().change_scene_to_file(level_path)
 	
-	await get_tree().process_frame
+	await SceneTransition.fade_in()
 	
 	get_tree().paused = false
 	
