98c331249c01d5a219695216ca4991682c39d55c
Gabriel Martinez <reitaka@gmail.com>
2025-05-16 12:18:41 -0700

    Show a dialogue indicator over NPCs
    When the player's interaction area overlaps with the NPCs' area.

--- entities/characters/samurai/samurai_blue.gd
+++ entities/characters/samurai/samurai_blue.gd
@@ -1,1 +1,7 @@
 class_name SamuraiBlue extends Character
+
+func _on_intereactable_selected() -> void:
+	$AnimatedSprite2D/InteractIndicator.visible = true
+
+func _on_intereactable_unselected() -> void:
+	$AnimatedSprite2D/InteractIndicator.visible = false
