9f7171f73af5ceedd8a4b7c2489cd535d9b62c11
jbuehler23 <jbuehler23@gmail.com>
2025-05-18 08:40:41 +0100

    feat: add basic respawner - there's an issue where the enemies all spawn at the same, i suspect the await is not doing what i think

---
+++ globals/spawners/enemy_spawn_data.gd
@@ -0,0 +1,5 @@
+class_name EnemySpawnData extends Resource
+
+@export var scene: PackedScene
+@export var spawn_delay: int = 1
+@export var max_number: int = 1
---
+++ globals/spawners/enemy_spawner.gd
@@ -0,0 +1,25 @@
+@tool
+class_name EnemySpawner extends Area2D
+
+@export var enemies: Array[EnemySpawnData]
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		sprite_2d.visibile = true
+	
+	for enemy_data in enemies:
+		var enemy = enemy_data.scene.instantiate() as Character
+		enemy.position = generate_random_position()
+		await get_tree().create_timer(enemy_data.spawn_delay).timeout
+		get_tree().current_scene.add_child(enemy)
+
+func generate_random_position() -> Vector2:
+	##assume collision shape is a circle:
+	var shape = collision_shape_2d.shape as CircleShape2D
+	var radius = shape.radius
+	var angle = randf() * TAU
+	var r = sqrt(randf()) * radius  # Uniform distribution
+	var offset = Vector2(cos(angle), sin(angle)) * r
+	return global_position + offset
