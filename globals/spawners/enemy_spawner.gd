d10bbb2ffe332c207ce87c07b69e95a6cef8b75d
jbuehler23 <jbuehler23@gmail.com>
2025-05-19 22:54:40 +0100

    feat: update dialogue and item interactions, enhance enemy spawner visibility, and refine item drop mechanics

--- globals/spawners/enemy_spawner.gd
+++ globals/spawners/enemy_spawner.gd
@@ -2,12 +2,13 @@
 class_name EnemySpawner extends Area2D
 
 @export var enemies: Array[EnemySpawnData]
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var sprite_2d = $Sprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 func _ready() -> void:
 	if Engine.is_editor_hint():
-		sprite_2d.visibile = true
+		sprite_2d.visible = true
+		return
 	
 	randomize()
 	for enemy_data in enemies:
--- shared/states/character/character_give_item.gd
+++ shared/states/character/character_give_item.gd
@@ -1,24 +1,32 @@
 class_name CharacterGiveItem extends State
 
-@export var anim_name: String = "give_item"
+@export var anim_name: String = "item_up"
+@export var item_to_give: ItemData
 @export_category("AI")
 @export var state_duration: float = 2.0
 @export var next_state: State
-@onready var item_to_give: Sprite2D = $"../../ItemToGive"
+
+const PICKUP = preload("res://entities/items/item_pickup.tscn")
 
 var _timer: float = 0.0
 
 func enter() -> void:
 	character.velocity = Vector2.ZERO
 	_timer = state_duration
-	character.animated_sprite_2d.play(anim_name + "_" + character.get_last_direction())
-	item_to_give.visible = true
+	character.animated_sprite_2d.play(anim_name)
+	character.animated_sprite_2d.animation_finished.connect(exit)
 
 func exit() -> void:
-	item_to_give.visible = false
+	drop_item()
 
 func physics_update(_delta: float) -> void:
 	_timer -= _delta
 	if _timer <= 0:
 		state_machine.change_state(next_state.name.to_lower())
 		
+func drop_item() -> void:
+	var drop: ItemPickup = PICKUP.instantiate() as ItemPickup
+	drop.set_item_data(item_to_give)
+	character.get_parent().call_deferred("add_child", drop)
+	drop.global_position = character.global_position
+	drop.velocity = character.velocity.rotated(randf_range(-1.5, 1.5)) * randf_range(0.9, 1.5)
