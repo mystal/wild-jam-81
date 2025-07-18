33b3cdbd8c42c1f5d749be0cbdc5c59be3b6a6bc
Gabriel Martinez <reitaka@gmail.com>
2025-07-17 20:11:36 -0700

    Fix biome warning for now

--- levels/biome/biome.gd
+++ levels/biome/biome.gd
@@ -1,9 +1,10 @@
 class_name Biome extends Area2D
 
-@export var weather_effects: Array[WeatherEffect] = []
+@export var weather_effects: Array[Variant] = []
+#@export var weather_effects: Array[WeatherEffect] = []
 
 func _ready() -> void:
     for effect in weather_effects:
         effect.start()
-    
+
 
